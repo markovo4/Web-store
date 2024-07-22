718eaf0e5cb924668e3f2772f95f5b16190a4494
markovo4 <obf2007@gmail.com>
2024-07-22 15:20:08 +0300

    Project set-up

--- src/components/Header/HeaderTop/HeaderTop.jsx
+++ src/components/Header/HeaderTop/HeaderTop.jsx
@@ -1,7 +1,17 @@
+import {styles} from "./styles.js";
+import {Button, Container} from "@mui/material";
+
 const HeaderTop = () => {
     return (
-        <div>HeaderTop</div>
-    )
+        <header style={styles.header}>
+            <Container sx={styles.container}>
+                <div style={styles.wrapper}>
+                    <Button sx={styles.button}>Log in</Button>
+                    <Button sx={styles.button}>Sign up</Button>
+                </div>
+            </Container>
+        </header>
+    );
 }
 
 export default HeaderTop;
---
+++ src/components/Header/HeaderTop/styles.js
@@ -0,0 +1,12 @@
+export const styles = {
+    header: {
+        height: '75px'
+    },
+    container: {
+        display: 'flex',
+        width: '1400px',
+        margin: '0 auto'
+    },
+    wrapper: {},
+    button: {}
+}
