9df302cb865da617cc8de912e5c4ad5e931b8f63
markovo4 <obf2007@gmail.com>
2024-07-23 06:59:00 +0300

    Header-Top&Bottom

--- src/components/Footer/FooterBottom/FooterBottom.jsx
+++ src/components/Footer/FooterBottom/FooterBottom.jsx
@@ -1,6 +1,25 @@
+import {Container, Typography} from "@mui/material";
+import {styles} from "./styles.js";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+
 const FooterBottom = () => {
     return (
-        <div>FooterBottom</div>
+        <section style={styles.footerBottom}>
+            <Container>
+                <Typography>© All Rights Reserved 2024</Typography>
+                <ul style={styles.socials}>
+                    <li><a href="#"><FacebookIcon style={styles.icon}/></a></li>
+                    <li><a href="#"><YouTubeIcon style={styles.icon}/></a></li>
+                    <li><a href="#"><InstagramIcon style={styles.icon}/></a></li>
+                    <li><a href="#"><TelegramIcon style={styles.icon}/></a></li>
+                    <li><a href="#"><PhoneInTalkIcon style={styles.icon}/></a></li>
+                </ul>
+            </Container>
+        </section>
     )
 }
 
---
+++ src/components/Footer/FooterBottom/styles.js
@@ -0,0 +1,11 @@
+export const styles = {
+    footerBottom: {},
+    socials: {},
+    icon: {
+        color: 'grey',
+        transition: 'color 0.3s',
+        '&:hover': {
+            color: 'black',
+        },
+    },
+}
