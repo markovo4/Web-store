08193177e86e2d10c54ec08163f3bf72ba926517
markovo4 <obf2007@gmail.com>
2024-08-18 11:07:27 +0300

    Additional Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux";
 import store from "./redux/store.js";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {SnackbarProvider} from "notistack";
@@ -12,11 +12,11 @@ import {SnackbarProvider} from "notistack";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <SnackbarProvider maxSnack={1}>
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                     <App/>
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         </SnackbarProvider>
     </React.StrictMode>,
 )
