590dad25c50c450ef497925a25d962b14452746a
markovo4 <obf2007@gmail.com>
2024-08-18 10:11:25 +0300

    Additional Fixes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'https://fakestoreapi.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-                secure: false, // if you're accessing a local dev server with an invalid cert
-            },
-        },
-    },
-})
+    base: './',
+    plugins: [
+        react()
+    ],
+});
