197ebac1a3903f263bbe663a666ea5a1caa223d3
markovo4 <obf2007@gmail.com>
2024-08-23 18:30:16 +0300

    Header Top styles Fix

--- src/components/Footer/FooterMiddle/styles.js
+++ src/components/Footer/FooterMiddle/styles.js
@@ -123,34 +123,4 @@ export const styles = {
         fontWeight: 'bold',
         color: 'rgba(0,0,0,0.72)'
     },
-    subtitle: {
-        fontSize: '15px',
-        color: 'rgba(0,0,0,0.65)',
-        '&:hover': {
-            textDecoration: 'underline',
-            color: 'rgba(0,0,0,0.85)',
-        }
-    },
-    phoneNumber: {
-        color: '#41ae2a',
-        fontWeight: 'bold',
-        fontSize: '22px',
-        '&:hover': {
-            textDecoration: 'underline'
-        }
-    },
-    phoneNumberLi: {
-        paddingTop: '0',
-        paddingBottom: '0',
-    },
-    linkServices: {
-        display: 'flex',
-        alignItems: 'center',
-        gap: '15px',
-
-        '&:hover': {
-            textDecoration: 'underline'
-        }
-    }
-
 };
