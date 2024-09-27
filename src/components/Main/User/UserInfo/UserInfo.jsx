90607f31ddc23b86bee4076f9ae093fc7895b68b
markovo4 <obf2007@gmail.com>
2024-09-27 16:58:29 +0300

    addition of favProducts array to each user recorded at LocalStorage

--- src/components/Main/User/UserInfo/UserInfo.jsx
+++ src/components/Main/User/UserInfo/UserInfo.jsx
@@ -1,12 +1,14 @@
 import {styles} from './styles.js';
-import {Avatar, Container, Typography} from "@mui/material";
+import {Avatar, Box, Container, Typography} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-import {getCurrentUser} from "../../../../redux/slices/localStorageSlice.js";
+import {getCurrentUser, getFavUserProductList, getListOfOrders} from "../../../../redux/slices/localStorageSlice.js";
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const UserInfo = () => {
 
-    const {currentUser} = useSelector(state => state.localStorage);
+    const {currentUser, listOfOrders = [], favouriteUserList = []} = useSelector(state => state.localStorage);
     const dispatch = useDispatch();
 
     const colorPicker = () => {
@@ -16,29 +18,45 @@ const UserInfo = () => {
             return `#${(a + b).toString(16).padStart(4, '0')}`;
         }
     }
-
     useEffect(() => {
+        dispatch(getFavUserProductList())
+        dispatch(getListOfOrders())
         dispatch(getCurrentUser())
     }, [dispatch])
+
+    const favouriteUserProducts = favouriteUserList
+        .filter(user => user.email === currentUser.email)
+        .flatMap(user => user.productsList || []);
 
     console.log(currentUser)
 
     return (
-        <Container sx={styles.container}>
-            {currentUser &&
+        currentUser && <Container sx={styles.container}>
+            <Box sx={styles.containerUser}>
                 <Avatar
                     sx={{
                         width: '100px',
                         height: '100px',
                         background: `${colorPicker()}`
-                    }}
-                >
+                    }}>
                     <Typography variant='h3' component='span' sx={styles.avatar}>
                         {`${currentUser.firstName.at(0).toUpperCase()}${currentUser.lastName.at(0).toUpperCase()}`}
                     </Typography>
                 </Avatar>
-            }
-            User Info
+                <Typography variant='h6' component='span' sx={styles.userFullName}>
+                    {currentUser.firstName} {currentUser.lastName}
+                </Typography>
+            </Box>
+
+
+            <Box sx={styles.personalStatsContainer}>
+                <Typography variant='h6' component='span' sx={styles.userFullName}>
+                    <ReceiptLongIcon/> My Orders {listOfOrders.length}
+                </Typography>
+                <Typography variant='h6' component='span' sx={styles.userFullName}>
+                    <FavoriteBorderIcon/> My Favourites {favouriteUserProducts.length}
+                </Typography>
+            </Box>
         </Container>
     )
 }
--- src/components/Main/User/UserInfo/styles.js
+++ src/components/Main/User/UserInfo/styles.js
@@ -1,3 +1,24 @@
 export const styles = {
-    container: {}
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '50px'
+    },
+    userFullName: {
+        fontSize: '25px',
+        color: '#0009',
+        fontWeight: '500'
+    },
+    personalStatsContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    containerUser: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '27%'
+    }
 }
